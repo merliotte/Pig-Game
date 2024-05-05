e94938ae77454f3d77026c3b1a310e9092f76a89
Merliotte <livainugent@hotmail.com>
2024-05-05 09:30:54 +0200

    create Fct InitGame

--- script.js
+++ script.js
@@ -11,10 +11,26 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let scores, currentScore, activePlayer, playing;
+
+const initGame = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = '0';
+  score1El.textContent = '0';
+  current0El.textContent = '0';
+  current1El.textContent = '0';
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--winner');
+};
+initGame();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -23,6 +39,7 @@ const switchPlayer = function () {
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
 };
+
 // Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
@@ -63,7 +80,7 @@ btnHold.addEventListener('click', function () {
     //2.Vérification score >= 100
     //   Fin du jeu
     document;
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
       diceEl.classList.add('hidden');
       document
@@ -81,10 +98,4 @@ btnHold.addEventListener('click', function () {
 
 // Réinitiailsation 2
 
-document.querySelector('.btn--new').addEventListener('click', function () {
-  score0El.textContent = '0';
-  score1El.textContent = '0';
-  current0El.textContent = '0';
-  current1El.textContent = '0';
-  diceEl.classList.add('hidden');
-});
+document.querySelector('.btn--new').addEventListener('click', initGame);
